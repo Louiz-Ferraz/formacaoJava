7d443c03242556f42d21816b7b1c3fd221e5e584
Louiz-Ferraz <luizfelipeferraz1@gmail.com>
2020-06-16 19:53:55 -0300

    Aula 1 de Maven
    Introdução ao Maven e seus goals, tudo ainda em linhas de comando

---
+++ Alura/calculadora-maven/src/main/Calculadora.java
@@ -0,0 +1,11 @@
+package main;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		
+		System.out.println("6 + 6 resulta em " + (6+6));
+		
+	}
+
+}
---
+++ Alura/calculadora-maven/src/test/CalculadoraTest.java
@@ -0,0 +1,11 @@
+package test;
+
+public class CalculadoraTest {
+
+	public static void main(String[] args) {
+		
+		
+		
+	}
+
+}
